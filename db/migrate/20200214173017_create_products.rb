8610462a9b8393901b70d7575d0ceef64e189503
kokoriru <ogihara.yuka.b@gmail.com>
2020-02-18 21:05:44 +0900

    products テーブル修正

--- db/migrate/20200214173017_create_products.rb
+++ db/migrate/20200214173017_create_products.rb
@@ -2,14 +2,18 @@ class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
       t.boolean :lowest, default: false, null: false
-      t.string :name
-      t.string :store
-      t.integer :price
-      t.integer :non_taxed_price
-      t.integer :shipping_fee
-      t.integer :point
-      t.integer :payment
-      t.text :description
+      t.string :name              # 品名
+      t.integer :quantity         # 数量
+      t.integer :weight           # 重量
+      t.string :quantity          # 単位
+      t.integer :price            # 税込金額
+      t.integer :non_taxed_price  # 税抜金額
+      t.integer :shipping_fee     # 送料
+      t.integer :point            # ポイント
+      t.integer :sum              # 合計金額
+      t.string :store             # ストア
+      t.string :url               # URL
+      t.text :description         # 備考
 
       t.timestamps
     end
